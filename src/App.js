ea3cb30a6351f01d8954547363f2a8c5834854f3
simon jerry <swagasoft@gmail.com>
2020-09-22 17:26:24 +0100

    movies table added

--- src/App.js
+++ src/App.js
@@ -9,69 +9,16 @@ import Movies from './components/movies';
 
 
 class App  extends Component {
-  state = { 
-    counters : [
-        {id: 1, value: 4},
-        {id: 2, value: 3},
-        {id: 3, value: 8},
-        {id: 4, value: 9},
-    ]
- }
 
- constructor(props){
-   console.log("App-contructor")
-    super(props);
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleReset = this.handleReset.bind(this);
- }
 
- componentDidMount(){
-   console.log('components is mounted')
- }
-
- handleReset(){
-    const counters =  this.state.counters.map(count => 
-        {count.value = 0;
-        return count});
-        this.setState({counters});
- }
-
- handleDelete(id){
-    console.log('event handler called', id);
-    // console.log('counting', this.state);
-    const newCounters = this.state.counters.filter(c => c.id !== id);
-    this.setState({counters : newCounters});
- }
-
- handleIncrement(id) {
-  this.props.counter.value += 1;
-  this.setState({value : this.props.counter.value});
-  
-  }
-
-  handleDecrement(){
-    this.props.counter.value -=1;
-    this.setState({value : this.props.counter.value});
-    this.state.places.forEach((place)=> {
-      console.log(place);
-    })
-    
-  }
 
 
   render(){
     console.log('conponent is rendered')
   return (
     <React.Fragment>
- <NavBar totalCounter={this.state.counters.filter(c => c.value > 0).length}></NavBar>
  <main className="container">
-   <Counters 
-   onReset={this.handleReset} 
-   onDelete={this.handleDelete} 
-   onIncrement={this.handleIncrement} 
-   onDecrement={this.handleDecrement} 
-   counters={this.state.counters}
-   ></Counters>
+  <Movies></Movies>
  </main>
  </React.Fragment>
   );
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import {getMovies} from  '../services/fakeMoviesService'
 
 class Movies extends Component {
-    state = {  }
+    state = { 
+        movies: getMovies()
+     }
+
+     handleDelete(movie){
+       const movies = this.state.movies.filter(m => m._id !== movie._id);
+       this.setState({movies})
+     }
     render() { 
-        return (  <h2>Movies Components</h2> );
+       const {length : count} = this.state.movies;
+       if(count === 0)
+        if(this.state.movies.length === 0) return <p>There no movies in the database.</p>
+        
+        return ( 
+            <React.Fragment>
+            <p> showing {count} Movies in the database </p>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Stock</th>
+                    <th>Rate</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.movies.map(movie => (
+                     <tr key={movie._id}>
+                     <td>{movie.title} </td>
+                     <td>{movie.genre.name} </td>
+                     <td>{movie.numberInStock} </td>
+                     <td>{movie.dailyRentalRate} </td>
+                     <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">delete</button></td>
+                 </tr>
+                ))}
+               
+            </tbody>
+        </table>
+        </React.Fragment>
+         );
     }
 }
  
